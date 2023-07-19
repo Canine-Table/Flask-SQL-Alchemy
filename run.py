57787da7a45b4eec1f0a962ade6866a98474d5ff
Canine Table <113630868+Canine-Table@users.noreply.github.com>
2023-07-19 03:07:56 -0400

    fix_2

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ from dotenv import load_dotenv
 from alchemy import create_app
 
 
-app = create_app()
-
-
 if __name__ == '__main__':
     load_dotenv()
-    app.run()
+    create_app = create_app()
+    create_app.run()
+else:
+    gunicorn_app = create_app()
