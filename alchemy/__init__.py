0fa5957322436d66b266964ec25a9b220c1407ff
Canine Table <113630868+Canine-Table@users.noreply.github.com>
2023-07-31 04:39:58 -0400

    updated to 5.3.1 d4

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -8,53 +8,45 @@ from flask_scss import Scss
 from flask import Flask
 
 
-bcrypt = Bcrypt()
-login_manager = LoginManager()
+app = Flask(__name__)
+app.config.from_object(Config)
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 login_manager.login_message_category = "info"
 login_manager.login_view = "account.login_page"
+Scss(app,static_dir=app.config['SCSS_STATIC_DIR'],asset_dir=app.config['SCSS_ASSET_DIR'])
 
 
-def create_app(specify_config=None):
-
-    app = Flask(__name__)
-
-    if specify_config == None:
-        app.config.from_object(Config)
-    else:
-        app.config.from_object(specify_config)
 
 
-    Jinja2Env.loaders.append(PackageLoader(package_name='alchemy', package_path='templates', encoding='utf-8'))
-    Jinja2Env.loaders.append(PackageLoader(package_name='alchemy.main', package_path='templates', encoding='utf-8'))
-    from alchemy.main.routes import main
-    app.register_blueprint(main)
+
+Jinja2Env.loaders.append(PackageLoader(package_name='alchemy', package_path='templates', encoding='utf-8'))
+Jinja2Env.loaders.append(PackageLoader(package_name='alchemy.main', package_path='templates', encoding='utf-8'))
+from alchemy.main.routes import main
+app.register_blueprint(main)
 
 
-    Jinja2Env.loaders.append(PackageLoader(package_name='alchemy.market', package_path='templates', encoding='utf-8'))
-    from alchemy.market.routes import market
-    app.register_blueprint(market)
+Jinja2Env.loaders.append(PackageLoader(package_name='alchemy.market', package_path='templates', encoding='utf-8'))
+from alchemy.market.routes import market
+app.register_blueprint(market)
 
 
-    Jinja2Env.loaders.append(PackageLoader(package_name='alchemy.accounts', package_path='templates', encoding='utf-8'))
-    from alchemy.accounts.routes import account
-    app.register_blueprint(account)
+Jinja2Env.loaders.append(PackageLoader(package_name='alchemy.accounts', package_path='templates', encoding='utf-8'))
+from alchemy.accounts.routes import account
+app.register_blueprint(account)
 
 
-    Jinja2Env.loaders.append(PackageLoader(package_name='alchemy.administration', package_path='templates', encoding='utf-8'))
-    from alchemy.administration.routes import administration
-    app.register_blueprint(administration)
+Jinja2Env.loaders.append(PackageLoader(package_name='alchemy.administration', package_path='templates', encoding='utf-8'))
+from alchemy.administration.routes import administration
+app.register_blueprint(administration)
 
 
-    Jinja2Env.loaders.append(PackageLoader(package_name='alchemy.information', package_path='templates', encoding='utf-8'))
-    from alchemy.information.routes import information
-    app.register_blueprint(information)
+Jinja2Env.loaders.append(PackageLoader(package_name='alchemy.information', package_path='templates', encoding='utf-8'))
+from alchemy.information.routes import information
+app.register_blueprint(information)
 
 
-    app.jinja_env = Jinja2Env(loader=ChoiceLoader(Jinja2Env.loaders))
+app.jinja_env = Jinja2Env(loader=ChoiceLoader(Jinja2Env.loaders))
 
 
-    login_manager.init_app(app)
-    bcrypt.init_app(app)
-    Scss(app,static_dir=app.config['SCSS_STATIC_DIR'],asset_dir=app.config['SCSS_ASSET_DIR'])
-
-    return app
--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from dotenv import load_dotenv
-from alchemy import create_app
+from alchemy import app
 
 
 if __name__ == '__main__':
     load_dotenv()
-    app =  create_app()
     app.run()
